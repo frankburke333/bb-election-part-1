26b97a07efc9dbfb6e02fbdf3dbceb9f6cba7e43
frankburke333 <frank.burke@gmail.com>
2017-02-17 14:37:31 -0500

    add bb completed  part 1 I'm writing in a vote for Mr Krabs

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+
+  }).done(function(data){
+    console.log(data);
+    for(var i=0; i < data.candidates.length; i++) {
+      $('#list').append('<li> Name: ' + data.candidates[i].name + '</li>')
+      $('#list').append('<li> Votes: ' + data.candidates[i].votes + '</li><br>')
+
+    }
+
+  });
+
+
 
 });
