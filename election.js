878dbd6cf6ae9bd3deb03ba725d76f8350c14eca
frankburke333 <frank.burke@gmail.com>
2017-02-25 10:55:26 -0500

    add completed bb election assignment. Used reload for results.  Need to learn update on click

--- election.js
+++ election.js
@@ -6,14 +6,37 @@ $(document).ready(function() {
 
   }).done(function(data){
     console.log(data);
+
+
     for(var i=0; i < data.candidates.length; i++) {
       $('#list').append('<li> Name: ' + data.candidates[i].name + '</li>')
-      $('#list').append('<li> Votes: ' + data.candidates[i].votes + '</li><br>')
-
+      $('#list').append('<li> Votes: ' + data.candidates[i].votes + '</li>')
+      $('#list').append('<form method="POST" class="vote" action="https://bb-election-api.herokuapp.com/vote"><input type="submit" > <input type="hidden" name="id" value='+data.candidates[i].id+'></form><br>')
     }
+    $('.vote').submit(function(event){
+        event.preventDefault();
+        var id = $(this).children('input[type=hidden]').val();
+        $.ajax({
+          url: 'https://bb-election-api.herokuapp.com/vote',
+          method: 'POST',
+          data: {id: id}
+
+        }).done(function(data){
+            console.log(data);
+            location.reload();
+
+        }).fail(function(jqXHR, textStatus, errorThrown){
+            console.log('Ajax Request Failed');
+            console.log(jqXHR);
+
+        }).always(function(){
+            console.log('Ajax Request Sent');
+        });
+
+
+    });
+
 
   });
-
-
 
 });
